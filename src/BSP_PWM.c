3c11e4de5255a284752afa67a1af1fe3bfb507d1
JordanAceto <jordanaceto@gmail.com>
2020-01-05 10:22:22 -0500

    update PWM module for more configurability

--- src/BSP_PWM.c
+++ src/BSP_PWM.c
@@ -3,7 +3,16 @@
 
 #include "PSP_GPIO.h"
 
-void BSP_PWM_Clock_Init(void)
+
+
+void BSP_PWM_Clock_Init_Default(void)
+{
+    BSP_PWM_Clock_Init(PWM_DEFAULT_CLOCK, PWM_DEFAULT_DIV);
+}
+
+
+
+void BSP_PWM_Clock_Init(BSP_PWM_Clock_Source_t clock_source, uint32_t divider)
 {
     // request a clock stop
     BSP_CM_PWMCTL_R = CM_PWMCTL_PASSWD | (BSP_CM_PWMCTL_R & (~CM_PWMCTL_ENAB));
@@ -14,13 +23,13 @@ void BSP_PWM_Clock_Init(void)
     }
     
     // set the divider, left shift of 12 pushes the divider setting into the integer part of the divider register
-    BSP_CM_PWMDIV_R = CM_PWMCTL_PASSWD | (PWM_DEFAULT_DIV << 12);
+    BSP_CM_PWMDIV_R = CM_PWMCTL_PASSWD | (divider << 12);
 
     // use internal oscillator clock source
-    BSP_CM_PWMCTL_R = CM_PWMCTL_PASSWD | CM_PWMCTL_USE_OSC;
+    BSP_CM_PWMCTL_R = CM_PWMCTL_PASSWD | clock_source;
 
     // request a clock start (datasheet says not to change the clock source and assert enable at the same time)
-    BSP_CM_PWMCTL_R = CM_PWMCTL_PASSWD | CM_PWMCTL_ENAB | CM_PWMCTL_USE_OSC;
+    BSP_CM_PWMCTL_R = CM_PWMCTL_PASSWD | CM_PWMCTL_ENAB | clock_source;
 
     while (!(BSP_CM_PWMCTL_R & CM_PWMCTL_BUSY))
     {
@@ -30,48 +39,58 @@ void BSP_PWM_Clock_Init(void)
 
 
 
-void BSP_PWM_Ch1_Start(void)
+void BSP_PWM_Channel_Start(BSP_PWM_Channel_t channel, BSP_PWM_Output_Mode_t mode, BSP_PWM_Range_t range)
 {
-    // set pwm control for channel 1 to mark/space mode and enable, leave channel 2 alone
-    BSP_PWM_CTL_R = BSP_PWM_CTL_R | (PWM_CTL_MSEN1 | PWM_CTL_PWEN1);
-
-    // set the range to the default value
-    BSP_PWM_RNG1_R = PWM_DEFAULT_RANGE;
+    if (channel == BSP_PWM_Channel_1)
+    {
+        // shift the mode into the MSEN1 position
+        BSP_PWM_CTL_R = 0xFF00u | (mode << 7u) | PWM_CTL_PWEN1;
+        BSP_PWM_RNG1_R = range;
+    }
+    else
+    {
+        // shift the mode into the MSEN2 position
+        BSP_PWM_CTL_R = 0x00FFu | (mode << 15u) | PWM_CTL_PWEN2;
+        BSP_PWM_RNG2_R = range;   
+    }
 }
 
-void BSP_PWM_Ch2_Start(void)
-{
-    // set pwm control for channel 2 to mark/space mode and enable, leave channel 1 alone
-    BSP_PWM_CTL_R = BSP_PWM_CTL_R | (PWM_CTL_MSEN2 | PWM_CTL_PWEN2);
 
-    // set the range to the default value
-    BSP_PWM_RNG2_R = PWM_DEFAULT_RANGE;
-}
 
 void BSP_PWM_Ch1_Write(uint32_t value)
 {
-    BSP_PWM_DAT1_R = value & PWM_RANGE_MASK;
+    BSP_PWM_DAT1_R = value;
 }
+
+
 
 void BSP_PWM_Ch2_Write(uint32_t value)
 {
-    BSP_PWM_DAT2_R = value & PWM_RANGE_MASK;
+    BSP_PWM_DAT2_R = value;
 }
+
+
 
 void BSP_PWM_Ch1_Set_GPIO12_To_PWM_Mode(void)
 {
     PSP_GPIO_Set_Pin_Mode(12u, PSP_GPIO_PINMODE_ALT0);
 }
 
+
+
 void BSP_PWM_Ch1_Set_GPIO18_To_PWM_Mode(void)
 {
     PSP_GPIO_Set_Pin_Mode(18u, PSP_GPIO_PINMODE_ALT5);
 }
 
+
+
 void BSP_PWM_Ch2_Set_GPIO13_To_PWM_Mode(void)
 {
     PSP_GPIO_Set_Pin_Mode(13u, PSP_GPIO_PINMODE_ALT0);
 }
+
+
 
 void BSP_PWM_Ch2_Set_GPIO19_To_PWM_Mode(void)
 {
--- src/BSP_PWM.h
+++ src/BSP_PWM.h
@@ -83,10 +83,52 @@
 #define CM_PWMCTL_USE_OSC    0x00000001u                               // CM PWMCTL use internal oscillator
 
 // PWM constants
-#define PWM_DEFAULT_RANGE    256u                                      // default PWM range [0, 255]
-#define PWM_RANGE_MASK       0xFFu                                     // used to get (val % PWM_DEFAULT_RANGE)
+#define PWM_DEFAULT_RANGE    BSP_PWM_RANGE_8_BITS                      // default PWM range [0, 255]
 
+#define PWM_DEFAULT_CLOCK    BSP_PWM_Clock_Source_OSCILLATOR           // default clock source, 19.2MHz internal osc
 #define PWM_DEFAULT_DIV      4u                                        // default clock divider, divide 19.2MHz clock by 4 = 4.8MHz
+
+
+
+/*------------------------------------------------------------------------------------------------
+    Public BSP_PWM Types
+ -------------------------------------------------------------------------------------------------*/
+ 
+typedef enum
+{
+    BSP_PWM_Clock_Source_GND = 0u,        // clock off
+    BSP_PWM_Clock_Source_OSCILLATOR = 1u, // internal oscillator runs at 19.2MHz
+    BSP_PWM_Clock_Source_PLL_A = 4u,      // defaults to off?
+    BSP_PWM_Clock_Source_PLL_C = 5u,      // defaults to about 990.799MHz?
+    BSP_PWM_Clock_Source_PLL_D = 6u,      // defaults to about 500MHz?
+    BSP_PWM_Clock_Source_HDMI_AUX = 7u    // defaults to about 216MHz?
+} BSP_PWM_Clock_Source_t;
+
+
+
+typedef enum
+{
+    BSP_PWM_Channel_1,
+    BSP_PWM_Channel_2
+} BSP_PWM_Channel_t;
+
+
+
+typedef enum
+{
+    BSP_PWM_N_OVER_M_MODE = 0u,   // use the N/M mode
+    BSP_PWM_MARK_SPACE_MODE = 1u  // use the M/S mode
+} BSP_PWM_Output_Mode_t;
+
+
+
+typedef enum
+{
+    BSP_PWM_RANGE_8_BITS  =  0x100u, // [0, 255]
+    BSP_PWM_RANGE_10_BITS =  0x400u, // [0, 1023]
+    BSP_PWM_RANGE_12_BITS =  0x1000, // [0, 4095]
+    BSP_PWM_RANGE_16_BITS = 0x10000, // [0, 65535]
+} BSP_PWM_Range_t;
 
 
 
@@ -98,12 +140,12 @@
 /*-----------------------------------------------------------------------------------------------
 
 Function Name:
-    BSP_PWM_Clock_Init
+    BSP_PWM_Clock_Init_Default
 
 Function Description:
-    Initialize the PWM clock source to internal oscillator divided down to 4.8MHz and start the PWM clock.
+    Initialize the PWM clock source to defauts of internal oscillator divided down to 4.8MHz, and start the PWM clock.
 
-    This function must be called before starting PWM channel 1 or 2, setting any GPIO pins to PWM mode or 
+    A clock init function must be called before starting PWM channel 1 or 2, setting any GPIO pins to PWM mode or 
     writing any pins via PWM.
 
 Inputs:
@@ -116,22 +158,24 @@ Error Handling:
     None
 
 -------------------------------------------------------------------------------------------------*/
-void BSP_PWM_Clock_Init(void);
+void BSP_PWM_Clock_Init_Default(void);
 
 
 
 /*-----------------------------------------------------------------------------------------------
 
 Function Name:
-    BSP_PWM_Ch1_Start
+    BSP_PWM_Clock_Init
 
 Function Description:
-    Initialize PWM Channel 1. Resolution is set to 8 bits at 18.75kHz.
-    
-    Note that only GPIO12 and GPIO18 are available as channel 1 PWM pins on the raspberry pi 3b+ breakout board.
+    Initialize the PWM clock source to a given clock source and divider.
+
+    A clock init  function must be called before starting PWM channel 1 or 2, setting any GPIO pins to PWM mode or 
+    writing any pins via PWM.
 
 Inputs:
-    None
+    clock source: the clock source to use
+    divider: the value to divide the base clock frequency by
 
 Returns:
     None
@@ -140,22 +184,26 @@ Error Handling:
     None
 
 -------------------------------------------------------------------------------------------------*/
-void BSP_PWM_Ch1_Start(void);
+void BSP_PWM_Clock_Init(BSP_PWM_Clock_Source_t clock_source, uint32_t divider);
+
 
 
 
 /*-----------------------------------------------------------------------------------------------
 
 Function Name:
-    BSP_PWM_Ch2_Start
+    BSP_PWM_Channel_Start
 
 Function Description:
-    Initialize PWM Channel 2. Resolution is set to 8 bits at 18.75kHz.
+    Initialize a given PWM channel by setting the ouput mode and range.
     
-    Note that only GPIO13 and GPIO19 are available as channel 2 PWM pins on the raspberry pi 3b+ breakout board.
+    Note that only GPIO12 and GPIO18 are available as channel 1 PWM pins and only 
+    GPIO13 and GPIO19 are available as channel 2 PWM pins on the raspberry pi 3b+ breakout board.
 
 Inputs:
-    None
+    channel: the channel to initialize, channel 1 or 2
+    mode: the output mode to use, PWM algorithm or M/S
+    range: the range of the PWM channel to use, 8, 10, 12, or 16 bits
 
 Returns:
     None
@@ -164,7 +212,8 @@ Error Handling:
     None
 
 -------------------------------------------------------------------------------------------------*/
-void BSP_PWM_Ch2_Start(void);
+void BSP_PWM_Channel_Start(BSP_PWM_Channel_t channel, BSP_PWM_Output_Mode_t mode, BSP_PWM_Range_t range);
+
 
 
 /*-----------------------------------------------------------------------------------------------
@@ -173,16 +222,16 @@ Function Name:
     BSP_PWM_Ch1_Write
 
 Function Description:
-    Writes a value to PWM Channel 1. Range is 8 bits.
+    Writes a value to PWM Channel 1.
 
 Inputs:
-    value: Value to write. Range [0,255]
+    value: the value to write.
 
 Returns:
     None
 
 Error Handling:
-    Values greater than 255 will be treated as (val % 256)
+    None
 
 -------------------------------------------------------------------------------------------------*/
 void BSP_PWM_Ch1_Write(uint32_t value);
@@ -195,16 +244,16 @@ Function Name:
     BSP_PWM_Ch2_Write
 
 Function Description:
-    Writes a value to PWM Channel 2. Range is 8 bits.
+    Writes a value to PWM Channel 2.
 
 Inputs:
-    r0: Value to write. Range [0,255]
+    value: the value to write.
 
 Returns:
     None
 
 Error Handling:
-    Values greater than 255 will be treated as (val % 256)
+    None
 
 -------------------------------------------------------------------------------------------------*/
 void BSP_PWM_Ch2_Write(uint32_t value);
