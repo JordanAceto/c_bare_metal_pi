0f6e73e42a5fb58d64c1d3fcac0d0d187fac6724
JordanAceto <jordanaceto@gmail.com>
2020-01-13 14:06:34 -0500

    move ILI9341 colors into public defines

--- src/BSP_ILI9341_SPI_Display.c
+++ src/BSP_ILI9341_SPI_Display.c
@@ -66,27 +66,6 @@
 #define BSP_ILI9341_GMCTRP1    0xE0u // Positive Gamma Correction
 #define BSP_ILI9341_GMCTRN1    0xE1u // Negative Gamma Correction
 
-// a few standard colors
-#define ILI9341_BLACK       0x0000u  //   0,   0,   0
-#define ILI9341_NAVY        0x000Fu  //   0,   0, 128
-#define ILI9341_DARKGREEN   0x03E0u  //   0, 128,   0
-#define ILI9341_DARKCYAN    0x03EFu  //   0, 128, 128
-#define ILI9341_MAROON      0x7800u  // 128,   0,   0
-#define ILI9341_PURPLE      0x780Fu  // 128,   0, 128
-#define ILI9341_OLIVE       0x7BE0u  // 128, 128,   0
-#define ILI9341_LIGHTGREY   0xC618u  // 192, 192, 192
-#define ILI9341_DARKGREY    0x7BEFu  // 128, 128, 128
-#define ILI9341_BLUE        0x001Fu  //   0,   0, 255
-#define ILI9341_GREEN       0x07E0u  //   0, 255,   0
-#define ILI9341_CYAN        0x07FFu  //   0, 255, 255
-#define ILI9341_RED         0xF800u  // 255,   0,   0
-#define ILI9341_MAGENTA     0xF81Fu  // 255,   0, 255
-#define ILI9341_YELLOW      0xFFE0u  // 255, 255,   0
-#define ILI9341_WHITE       0xFFFFu  // 255, 255, 255
-#define ILI9341_ORANGE      0xFD20u  // 255, 165,   0
-#define ILI9341_GREENYELLOW 0xAFE5u  // 173, 255,  47
-#define ILI9341_PINK        0xF81Fu
-
 #define ILI9341_DC_PIN_WRITE_COMMAND 0u
 #define ILI9341_DC_PIN_WRITE_DATA    1u
 
--- src/BSP_ILI9341_SPI_Display.h
+++ src/BSP_ILI9341_SPI_Display.h
@@ -32,6 +32,27 @@
 #define BSP_ILI9341_TFTWIDTH  240u
 #define BSP_ILI9341_TFTHEIGHT 320u
 
+// a few standard colors
+#define ILI9341_BLACK       0x0000u
+#define ILI9341_NAVY        0x000Fu
+#define ILI9341_DARKGREEN   0x03E0u
+#define ILI9341_DARKCYAN    0x03EFu
+#define ILI9341_MAROON      0x7800u
+#define ILI9341_PURPLE      0x780Fu
+#define ILI9341_OLIVE       0x7BE0u
+#define ILI9341_LIGHTGREY   0xC618u
+#define ILI9341_DARKGREY    0x7BEFu
+#define ILI9341_BLUE        0x001Fu
+#define ILI9341_GREEN       0x07E0u
+#define ILI9341_CYAN        0x07FFu
+#define ILI9341_RED         0xF800u
+#define ILI9341_MAGENTA     0xF81Fu
+#define ILI9341_YELLOW      0xFFE0u
+#define ILI9341_WHITE       0xFFFFu
+#define ILI9341_ORANGE      0xFD20u
+#define ILI9341_GREENYELLOW 0xAFE5u
+#define ILI9341_PINK        0xF81Fu
+
 
 /*-----------------------------------------------------------------------------------------------
     Public BSP_ILI9341_SPI_Display Function Declarations
