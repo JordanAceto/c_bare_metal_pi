31732ff9fbf3010bb383223188eff872b92d9a4e
JordanAceto <jordanaceto@gmail.com>
2020-01-20 14:54:41 -0500

    remove I2C union/struct access

--- src/PSP_I2C.c
+++ src/PSP_I2C.c
@@ -53,159 +53,6 @@
 
 
 /*-----------------------------------------------------------------------------------------------
-    Private PSP_I2C Types
- -------------------------------------------------------------------------------------------------*/
-
-
-/* I2C Control Register Type */
-typedef union I2C_Control_Reg_Type
-{
-    struct
-    {
-        vuint32_t READ  : 1u;  // Read Transfer. 0 = Write Packet Transfer. 1 = Read Packet Transfer.
-        vuint32_t       : 3u;  // Reserved - Write as 0, read as don't care
-        vuint32_t CLEAR : 2u;  // Clear FIFO. 00 = No action. x1 = Clear FIFO. 1x = Clear FIFO
-        vuint32_t       : 1u;  // Reserved - Write as 0, read as don't care
-        vuint32_t ST    : 1u;  // Start Transfer 0 = No action. 1 = Start a new transfer.
-        vuint32_t INTD  : 1u;  // Interrupt on DONE. 0 = Don t generate interrupts on DONE condition. 1 = do.
-        vuint32_t INTT  : 1u;  // Interrupt on TX. 0 = Don t generate interrupts on TXW condition. 1 = do.
-        vuint32_t INTR  : 1u;  // Interrupt on RX. 0 = Don t generate interrupts on RXR condition. 1 = do.
-        vuint32_t       : 4u;  // Reserved - Write as 0, read as don't care
-        vuint32_t I2CEN : 1u;  // I2C Enable. 0 = BSC controller is disabled. 1 = BSC controller is enabled
-        vuint32_t       : 16u; // Reserved - Write as 0, read as don't care
-    };
-    
-    vuint32_t DATA; // Read/write the entire 32 bit word at once.
-
-} I2C_Control_Register_t;
-
-
-
-/* I2C Status Register Type */
-typedef union I2C_Status_Reg_Type
-{
-    struct
-    {
-        vuint32_t TA   : 1u;  // Transfer Active. 0 = Transfer not active. 1 = Transfer active.
-        vuint32_t DONE : 1u;  // Transfer Done. 0 = not done. 1 = done. Cleared by writing 1 to the field.
-        vuint32_t TXW  : 1u;  // FIFO needs Writing.
-        vuint32_t RXR  : 1u;  // FIFO needs Reading. 
-        vuint32_t TXD  : 1u;  // FIFO can accept Data. 0 = FIFO is full. 1 = FIFO has space for at least 1 byte.
-        vuint32_t RXD  : 1u;  // FIFO contains Data. 0 = FIFO is empty. 1 = FIFO contains at least 1 byte.
-        vuint32_t TXE  : 1u;  // FIFO Empty. 0 = FIFO is not empty. 1 = FIFO is empty.
-        vuint32_t RXF  : 1u;  // FIFO Full. 0 = FIFO is not full. 1 = FIFO is full.
-        vuint32_t ERR  : 1u;  // ACK Error. 0 = No errors detected. 1 = Slave has not acknowledged its address.
-        vuint32_t CLKT : 1u;  // Clock Stretch Timeout. 0 = No errors detected. 1 = clock stretch timeout error.
-        vuint32_t      : 22u; // Reserved - Write as 0, read as don't care
-    };
-    
-    vuint32_t DATA; // Read/write the entire 32 bit word at once.
-
-} I2C_Status_Register_t;
-
-
-
-/* I2C Data Length Register Type */
-typedef union I2C_DLEN_Reg_Type
-{
-    struct
-    {
-        vuint32_t DATA     : 16u; // Data Length. The number of bytes to transfer.
-        vuint32_t RESERVED : 16u; // Reserved - Write as 0, read as don't care
-    };
-    
-} I2C_DLEN_Regsiter_t;
-
-
-
-/* I2C Slave Address Register Type */
-typedef union I2C_Address_Reg_Type
-{
-    struct
-    {
-        vuint32_t DATA     :  7u; // Slave Address.
-        vuint32_t RESERVED : 25u; // Reserved - Write as 0, read as don't care
-    };
-    
-} I2C_Address_Regsiter_t;
-
-
-
-/* I2C FIFO Register Type */
-typedef union I2C_FIFO_Reg_Type
-{
-    struct
-    {
-        vuint32_t DATA     :  8u; // 16 byte FIFO
-        vuint32_t RESERVED : 24u; // Reserved - Write as 0, read as don't care
-    };
-    
-} I2C_FIFO_Regsiter_t;
-
-
-
-/* I2C DIV Register Type */
-typedef union I2C_DIV_Reg_Type
-{
-    struct
-    {
-        vuint32_t CDIV     : 16u; // Clock Divider. SCL = core clock / CDIV Where core_clk is nominally 150 MHz.
-        vuint32_t RESERVED : 16u; // Reserved - Write as 0, read as don't care
-    };
-    
-} I2C_DIV_Regsiter_t;
-
-
-
-/* I2C DEL Register Type */
-typedef union I2C_DEL_Reg_Type
-{
-    struct
-    {
-        vuint32_t REDL : 16u; // Falling Edge Delay. Num clock cycles to wait after rising edge of SCL to output data.
-        vuint32_t FEDL : 16u; // Falling Edge Delay. Num clock cycles to wait after falling edge of SCL to output data.
-    };
-    
-} I2C_DEL_Regsiter_t;
-
-
-
-/* I2C CLKT Register Type */
-typedef union I2C_CLKT_Reg_Type
-{
-    struct
-    {
-        vuint32_t TOUT     : 16u; // Clock Stretch Timeout Value.
-        vuint32_t RESERVED : 16u; // Reserved - Write as 0, read as don't care
-    };
-    
-} I2C_CLKT_Regsiter_t;
-
-
-
-typedef struct I2C_Type
-{
-    I2C_Control_Register_t C;    // Control Register
-    I2C_Status_Register_t  S;    // Status Register
-    I2C_DLEN_Regsiter_t    DLEN; // Data Length Register 
-    I2C_Address_Regsiter_t ADDR; // Slave Address Register
-    I2C_FIFO_Regsiter_t    FIFO; // FIFO Register
-    I2C_DIV_Regsiter_t     DIV;  // Clock Divider Register
-    I2C_DEL_Regsiter_t     DEL;  // Data Delay Register
-    I2C_CLKT_Regsiter_t    CLKT; // Clock Stretch Timout Register
-} I2C_t;
-
-
-
-/*-----------------------------------------------------------------------------------------------
-    Private PSP_I2C Variables
- -------------------------------------------------------------------------------------------------*/
-
-volatile I2C_t * I2C = (volatile I2C_t *)PSP_I2C_BASE_A;
-
-
-
-/*-----------------------------------------------------------------------------------------------
     PSP_I2C Function Definitions
  -------------------------------------------------------------------------------------------------*/
 
@@ -228,14 +75,14 @@ void PSP_I2C_Start(void)
 
 void PSP_I2C_Set_Clock_Divider(uint32_t divider)
 {
-    I2C->DIV.CDIV = divider;
+    PSP_I2C_DIV_R = divider;
 }
 
 
 
 void PSP_I2C_Set_Slave_Address(uint32_t address)
 {
-    I2C->ADDR.DATA = address;
+    PSP_I2C_SA_R = address;
 }
 
 
@@ -243,29 +90,26 @@ void PSP_I2C_Set_Slave_Address(uint32_t address)
 void PSP_I2C_Write_Byte(uint8_t val)
 {
     // clear the fifo
-    I2C->C.CLEAR = 1u;
+    PSP_I2C_C_R |= I2C_C_CLEAR_1;
 
     // clear the clock stretch timeout, no acknowledge error, and transfer done status flags 
     // note that these flags are cleared by writing a 1
-    I2C->S.CLKT = 1u;
-    I2C->S.ERR  = 1u;
-    I2C->S.DONE = 1u;
+    PSP_I2C_S_R |= I2C_S_CLKT | I2C_S_ERR | I2C_S_DONE;
     
     // set data length to 1 byte
-    I2C->DLEN.DATA = 1u;
+    PSP_I2C_DLEN_R = 1u;
 
     // write the value to the fifo
-    I2C->FIFO.DATA = val;
+    PSP_I2C_FIFO_R = val;
     
     // enable device and start transfer
-    I2C->C.I2CEN = 1u;
-    I2C->C.ST    = 1u;
+    PSP_I2C_C_R |= I2C_C_I2CEN | I2C_C_ST;
 
-    while (I2C->S.DONE == 0u)
+    while (!(PSP_I2C_S_R & I2C_S_DONE))
     {
         // wait for transfer to complete
     }
 
     // set the done flag inorder to clear it and end the transfer
-    I2C->S.DONE = 1u; // is this necessary?
+    PSP_I2C_S_R |= I2C_S_DONE;
 }
