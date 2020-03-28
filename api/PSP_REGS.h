e46e965ef7ad459caf944709d68ef9ebc4c471ad
JordanAceto <jordanaceto@gmail.com>
2020-03-28 14:28:32 -0400

    update PSP_REGS to new file standards

--- api/PSP_REGS.h
+++ api/PSP_REGS.h
@@ -1,27 +1,45 @@
-/**
- * DESCRIPTION:
- *      PSP_REGS simply keeps a publicly available list of register addresses,
- *      as well as the peripheral base address.
- * 
- * NOTES:
- *      TODO: For now this only works with the Raspberry Pi 3b+ and other Pis with 
- *      0x3F000000u as their peripheral base address. Add #ifdef statements
- *      so that it can work with other hardware.
- * 
- * REFERENCES:
- *      BCM2837-ARM-Peripherals.pdf 
- */
+/*
+--|----------------------------------------------------------------------------|
+--| FILE DESCRIPTION:
+--|   PSP_REGS simply keeps a publicly available list of register addresses,
+--|   as well as the peripheral base address.
+--|  
+--|----------------------------------------------------------------------------|
+--| REFERENCES:
+--|   BCM2837-ARM-Peripherals.pdf 
+--|
+--|----------------------------------------------------------------------------|
+*/
 
 #ifndef PSP_REGS_H_INCLUDED
 #define PSP_REGS_H_INCLUDED
 
-/*------------------------------------------------------------------------------------------------
-    Public PSP_REGS Defines
- -------------------------------------------------------------------------------------------------*/
+/*
+--|----------------------------------------------------------------------------|
+--| INCLUDE FILES
+--|----------------------------------------------------------------------------|
+*/
 
-// Register Base Addresses
+/* None */
+
+/*
+--|----------------------------------------------------------------------------|
+--| PUBLIC DEFINES
+--|----------------------------------------------------------------------------|
+*/
+
+/*
+--| NAME: PSP_REGS_PERIPHERAL_BASE_ADDRESS
+--| DESCRIPTION: peripheral base address
+--| TYPE: uint32_t
+*/
 #define PSP_REGS_PERIPHERAL_BASE_ADDRESS (0x3F000000u)
 
+/*
+--| NAME: PSP_REGS_xxx_ADDRESS
+--| DESCRIPTION: base address of a given peripheral
+--| TYPE: uint32_t
+*/
 #define PSP_REGS_GPIO_BASE_ADDRESS         (PSP_REGS_PERIPHERAL_BASE_ADDRESS | 0x00200000u)
 #define PSP_REGS_SYSCLK_BASE_ADDRESS       (PSP_REGS_PERIPHERAL_BASE_ADDRESS | 0x00003000u)
 #define PSP_REGS_PWM_BASE_ADDRESS          (PSP_REGS_PERIPHERAL_BASE_ADDRESS | 0x0020C000u) 
@@ -29,5 +47,37 @@
 #define PSP_REGS_I2C_BASE_ADDRESS          (PSP_REGS_PERIPHERAL_BASE_ADDRESS | 0x00804000u)
 #define PSP_REGS_AUX_BASE_ADDRESS          (PSP_REGS_PERIPHERAL_BASE_ADDRESS | 0x00215000u)
 #define PSP_REGS_HARDWARE_RNG_BASE_ADDRESS (PSP_REGS_PERIPHERAL_BASE_ADDRESS | 0x00104000u)
+
+/*
+--|----------------------------------------------------------------------------|
+--| PUBLIC TYPES
+--|----------------------------------------------------------------------------|
+*/
+
+/* None */
+
+/*
+--|----------------------------------------------------------------------------|
+--| PUBLIC CONSTANTS
+--|----------------------------------------------------------------------------|
+*/
+
+/* None */
+
+/*
+--|----------------------------------------------------------------------------|
+--| PUBLIC VARIABLES
+--|----------------------------------------------------------------------------|
+*/
+
+/* None */
+
+/*
+--|----------------------------------------------------------------------------|
+--| PUBLIC FUNCTION PROTOTYPES
+--|----------------------------------------------------------------------------|
+*/
+
+/* None */
 
 #endif
