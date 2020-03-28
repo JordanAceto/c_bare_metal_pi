70a4b0a06efb80e7eb278da7edbfdf3a2048505b
JordanAceto <jordanaceto@gmail.com>
2020-03-28 17:59:16 -0400

    update hardware RNG to new code standards

--- api/PSP_Hardware_RNG.h
+++ api/PSP_Hardware_RNG.h
@@ -1,35 +1,78 @@
-/**
- * DESCRIPTION:
- *      PSP_Hardware_PWM provides an interface for the built in hardware random 
- *      number generator.
- * 
- * NOTES:
- *      The hardware random number generator is not well documented. This module was
- *      developed with hints from the web.
- * 
- * REFERENCES:
- *      https://www.raspberrypi.org/forums/viewtopic.php?t=196015
- *      https://elinux.org/BCM2835_registers#RNG (wrong processor, but seems to work)
- */
+/*
+--|----------------------------------------------------------------------------|
+--| FILE DESCRIPTION:
+--|   PSP_Hardware_PWM provides an interface for the built in hardware random 
+--|   number generator.
+--|  
+--|----------------------------------------------------------------------------|
+--| NOTES:
+--|    The hardware random number generator is not well documented. This module 
+--|    was developed with hints from the web.
+--|  
+--|----------------------------------------------------------------------------|
+--| REFERENCES:
+--|    https://www.raspberrypi.org/forums/viewtopic.php?t=196015
+--|    https://elinux.org/BCM2835_registers#RNG (wrong processor, but seems to work)
+--|
+--|----------------------------------------------------------------------------|
+*/
 
 #ifndef PSP_HARDWARE_RNG_H_INCLUDED
 #define PSP_HARDWARE_RNG_H_INCLUDED
 
+/*
+--|----------------------------------------------------------------------------|
+--| INCLUDE FILES
+--|----------------------------------------------------------------------------|
+*/
+
 #include "Fixed_Width_Ints.h"
 
-/*------------------------------------------------------------------------------------------------
-    Public PSP_Hardware_RNG Function Declarations
- -------------------------------------------------------------------------------------------------*/
+/*
+--|----------------------------------------------------------------------------|
+--| PUBLIC DEFINES
+--|----------------------------------------------------------------------------|
+*/
 
+/* None */
 
-/*-----------------------------------------------------------------------------------------------
+/*
+--|----------------------------------------------------------------------------|
+--| PUBLIC TYPES
+--|----------------------------------------------------------------------------|
+*/
 
+/* None */
+
+/*
+--|----------------------------------------------------------------------------|
+--| PUBLIC CONSTANTS
+--|----------------------------------------------------------------------------|
+*/
+
+/* None */
+
+/*
+--|----------------------------------------------------------------------------|
+--| PUBLIC VARIABLES
+--|----------------------------------------------------------------------------|
+*/
+
+/* None */
+
+/*
+--|----------------------------------------------------------------------------|
+--| PUBLIC FUNCTION PROTOTYPES
+--|----------------------------------------------------------------------------|
+*/
+
+/*------------------------------------------------------------------------------
 Function Name:
     PSP_Hardware_RNG_Init
 
 Function Description:
-    Initialize the hardware RNG source. The exact workings of the RNG source are not well
-    documented or understood.
+    Initialize the hardware RNG source. The exact workings of the RNG source 
+    are not well documented or understood.
 
 Inputs:
     None
@@ -37,16 +80,12 @@ Inputs:
 Returns:
     None
 
-Error Handling:
+Assumptions/Limitations:
     None
-
--------------------------------------------------------------------------------------------------*/
+------------------------------------------------------------------------------*/
 void PSP_Hardware_RNG_Init(void);
 
-
-
-/*-----------------------------------------------------------------------------------------------
-
+/*------------------------------------------------------------------------------
 Function Name:
     PSP_Hardware_RNG_Get_Random
 
@@ -59,11 +98,9 @@ Inputs:
 Returns:
     A random uint32_t.
 
-Error Handling:
+Assumptions/Limitations:
     None, assumes that PSP_Hardware_RNG_Init has been called.
-
--------------------------------------------------------------------------------------------------*/
+------------------------------------------------------------------------------*/
 uint32_t PSP_Hardware_RNG_Get_Random(void);
-
 
 #endif
