a2223193bd7e21d2b2477df657bf0ffa278a63ae
JordanAceto <jordanaceto@gmail.com>
2020-03-29 14:49:00 -0400

    update rotary encoder to new file standards

--- api/BSP_Rotary_Encoder.h
+++ api/BSP_Rotary_Encoder.h
@@ -1,37 +1,56 @@
-/**
- * DESCRIPTION:
- *      BSP_Rotaty_Encoder provides types and interfaces for connecting
- *      and reading rotary encoders.
- * 
- * NOTES:
- *      This module has been tested with inexpensive Bourns PEC11R series
- *      encoders using the example circuit in the datasheet. External resistors
- *      and capacitors were used.
- * 
- *      Other encoders may or may not require modifications.
- * 
- *      While testing, I just spun the encoder by hand. I don't know if this
- *      module would keep up with an encoder on a motor shaft or something.
- * 
- * REFERENCES:
- *      https://www.bourns.com/docs/Product-Datasheets/PEC11R.pdf
- */
+/*
+--|----------------------------------------------------------------------------|
+--| FILE DESCRIPTION:
+--|     BSP_Rotaty_Encoder provides types and interfaces for connecting
+--|     and reading rotary encoders.
+--|  
+--|----------------------------------------------------------------------------|
+--| NOTES:
+--|     This module has been tested with inexpensive Bourns PEC11R series
+--|     encoders using the example circuit in the datasheet. External resistors
+--|     and capacitors were used.
+--|
+--|     Other encoders may or may not require modifications.
+--|
+--|     While testing, I just spun the encoder by hand. I don't know if this
+--|     module would keep up with an encoder on a motor shaft or something.
+--|  
+--|----------------------------------------------------------------------------|
+--| REFERENCES:
+--|   https://www.bourns.com/docs/Product-Datasheets/PEC11R.pdf
+--|
+--|----------------------------------------------------------------------------|
+*/
 
 #ifndef BSP_ROTARY_ENCODER_H_INCLUDED
 #define BSP_ROTARY_ENCODER_H_INCLUDED
 
+/*
+--|----------------------------------------------------------------------------|
+--| INCLUDE FILES
+--|----------------------------------------------------------------------------|
+*/
+
 #include "Fixed_Width_Ints.h"
 
+/*
+--|----------------------------------------------------------------------------|
+--| PUBLIC DEFINES
+--|----------------------------------------------------------------------------|
+*/
 
-/*-----------------------------------------------------------------------------------------------
-    Public BSP_Rotary_Encoder Defines
- -------------------------------------------------------------------------------------------------*/
+/* None */
 
+/*
+--|----------------------------------------------------------------------------|
+--| PUBLIC TYPES
+--|----------------------------------------------------------------------------|
+*/
 
- /*-----------------------------------------------------------------------------------------------
-    Public BSP_Rotary_Encoder Types
- -------------------------------------------------------------------------------------------------*/
-
+/*
+--| NAME: BSP_Rotary_Encoder_State_t
+--| DESCRIPTION: storage for the state of a rotary encoder
+*/
 typedef union Rotary_Encoder_State_Type
 {
     struct
@@ -40,12 +59,15 @@ typedef union Rotary_Encoder_State_Type
         uint8_t Pin_B_State      : 1u;
         uint8_t Last_Pin_A_State : 1u;
         uint8_t Last_Pin_B_State : 1u;
-        uint8_t                  : 4u; // unused
+        uint8_t UNUSED           : 4u;
     };
     uint8_t Raw_Data; // the encoder state is stored in the lower 4 bits
 } BSP_Rotary_Encoder_State_t;
 
-
+/*
+--| NAME: BSP_Rotary_Encoder_t
+--| DESCRIPTION: storage for a rotary encoder struct
+*/
 typedef struct Rotary_Encoder_Type
 {
     const uint32_t PIN_A;
@@ -57,20 +79,35 @@ typedef struct Rotary_Encoder_Type
 
 } BSP_Rotary_Encoder_t;
 
+/*
+--|----------------------------------------------------------------------------|
+--| PUBLIC CONSTANTS
+--|----------------------------------------------------------------------------|
+*/
 
+/* None */
 
- /*-----------------------------------------------------------------------------------------------
-    Public BSP_Rotary_Encoder Function Declarations
- -------------------------------------------------------------------------------------------------*/
+/*
+--|----------------------------------------------------------------------------|
+--| PUBLIC VARIABLES
+--|----------------------------------------------------------------------------|
+*/
 
-/*-----------------------------------------------------------------------------------------------
+/* None */
 
+/*
+--|----------------------------------------------------------------------------|
+--| PUBLIC FUNCTION PROTOTYPES
+--|----------------------------------------------------------------------------|
+*/
+
+/*------------------------------------------------------------------------------
 Function Name:
     BSP_Rotary_Encoder_Initialize
 
 Function Description:
-    Initialize a rotary encoder by setting its A and B pins to inputs, setting its 
-    count to zero, and setting its state to zero.
+    Initialize a rotary encoder by setting its A and B pins to inputs, 
+    setting its count to zero, and setting its state to zero.
 
 Inputs:
     pEncoder: pointer to the encoder to initialize.
@@ -78,37 +115,33 @@ Inputs:
 Returns:
     None
 
-Error Handling:
+Assumptions/Limitations:
     None
 
--------------------------------------------------------------------------------------------------*/
+------------------------------------------------------------------------------*/
 void BSP_Rotary_Encoder_Initialize(BSP_Rotary_Encoder_t * pEncoder);
 
-
-
-/*-----------------------------------------------------------------------------------------------
-
+/*------------------------------------------------------------------------------
 Function Name:
     BSP_Poll_Rotary_Encoder
 
 Function Description:
     Polls a rotary encoder and updates its state and count.
 
-    Note that this function must be called as frequently as possible in order to 
-    accurately poll the encoder.
+    Note that this function must be called as frequently as possible in order 
+    to accurately poll the encoder.
 
 Inputs:
-    pEncoder: pointer to the encoder to poll. The state of this encoder will be modified
-    during the function call.
+    pEncoder: pointer to the encoder to poll. The state of this encoder will 
+    be modified during the function call.
 
 Returns:
     The updated encoder state and count in pEncoder.
 
-Error Handling:
+Assumptions/Limitations:
     None
 
--------------------------------------------------------------------------------------------------*/
+------------------------------------------------------------------------------*/
 void BSP_Poll_Rotary_Encoder(BSP_Rotary_Encoder_t * pEncoder);
-
 
 #endif
--- src/BSP_Rotary_Encoder.c
+++ src/BSP_Rotary_Encoder.c
@@ -1,51 +1,94 @@
+/*
+--|----------------------------------------------------------------------------|
+--| FILE DESCRIPTION:
+--|   BSP_Rotaty_Encoder.c provides the implementation for connecting
+--|   and reading rotary encoders.
+--|  
+--|----------------------------------------------------------------------------|
+--| REFERENCES:
+--|   https://www.bourns.com/docs/Product-Datasheets/PEC11R.pdf
+--|
+--|----------------------------------------------------------------------------|
+*/
+
+/*
+--|----------------------------------------------------------------------------|
+--| INCLUDE FILES
+--|----------------------------------------------------------------------------|
+*/
 
 #include "BSP_Rotary_Encoder.h"
 #include "PSP_GPIO.h"
 
-
- /*-----------------------------------------------------------------------------------------------
-    Private BSP_Rotary_Encoder Variables
- -------------------------------------------------------------------------------------------------*/
 /*
-    To read a given encoder, its current pin A and B state as well as its last pin A and B states
-    are maintained in a struct along with other information about the encoder.
+--|----------------------------------------------------------------------------|
+--| PRIVATE DEFINES
+--|----------------------------------------------------------------------------|
+*/
 
-    For any given transition of current/last pin states the encoder's counter should either
-    count up, count down, or stay the same.
+/* None */
 
-    The table of encoder-state to desired-counter-increment is below:
+/*
+--|----------------------------------------------------------------------------|
+--| PRIVATE TYPES
+--|----------------------------------------------------------------------------|
+*/
 
-    (a0/b0 represent the last encoder state, and a1/b1 represent the current encoder state)
+/* None */
 
-    a0  |  b0 |  a1 |  b1 |  result
-    --------------------------------
-     0  |  0  |  0  |  0  |    0
-     0  |  0  |  0  |  1  |    1
-     0  |  0  |  1  |  0  |   -1
-     0  |  0  |  1  |  1  |    0
+/*
+--|----------------------------------------------------------------------------|
+--| PRIVATE CONSTANTS
+--|----------------------------------------------------------------------------|
+*/
 
-     0  |  1  |  0  |  0  |   -1
-     0  |  1  |  0  |  1  |    0
-     0  |  1  |  1  |  0  |    0
-     0  |  1  |  1  |  1  |    1
-
-     1  |  0  |  0  |  0  |    1
-     1  |  0  |  0  |  1  |    0
-     1  |  0  |  1  |  0  |    0
-     1  |  0  |  1  |  1  |   -1
-
-     1  |  1  |  0  |  0  |    0
-     1  |  1  |  0  |  1  |   -1
-     1  |  1  |  1  |  0  |    1
-     1  |  1  |  1  |  1  |    0
-
-    The information in the above table is stored in the STATE_TRANSITIONS array below,
-    where the four-bit state of the encoder is used as the index into the array, and
-    thre result is stored as the value at that index.
-
-    Because the encoder state is stored in a union, we can use this to directly index
-    into the below array, and retrieve the correct counter increment for the given
-    encoder state.
+/*
+--| NAME: STATE_TRANSITIONS
+--| DESCRIPTION: array of state transitions for a rotary encoder
+--| TYPE: int8_t
+--|     To read a given encoder, its current pin A and B state as well as its 
+--|     last pin A and B states are maintained in a struct along with other 
+--|     information about the encoder.
+--|
+--|     For any given transition of current/last pin states the encoder's counter 
+--|     should either count up, count down, or stay the same.
+--|
+--|     The table of encoder-state to desired-counter-increment is below:
+--|
+--|     (a0/b0 represent the last encoder state, and a1/b1 represent the current 
+--|     encoder state)
+--|
+--|    a0  |  b0 |  a1 |  b1 |  result
+--|    --------------------------------
+--|     0  |  0  |  0  |  0  |    0
+--|     0  |  0  |  0  |  1  |    1
+--|     0  |  0  |  1  |  0  |   -1
+--|     0  |  0  |  1  |  1  |    0
+--|
+--|     0  |  1  |  0  |  0  |   -1
+--|     0  |  1  |  0  |  1  |    0
+--|     0  |  1  |  1  |  0  |    0
+--|     0  |  1  |  1  |  1  |    1
+--|
+--|     1  |  0  |  0  |  0  |    1
+--|     1  |  0  |  0  |  1  |    0
+--|     1  |  0  |  1  |  0  |    0
+--|     1  |  0  |  1  |  1  |   -1
+--|
+--|     1  |  1  |  0  |  0  |    0
+--|     1  |  1  |  0  |  1  |   -1
+--|     1  |  1  |  1  |  0  |    1
+--|     1  |  1  |  1  |  1  |    0
+--|
+--|     The information in the above table is stored in the STATE_TRANSITIONS 
+--|     array below, where the four-bit state of the encoder is used as the 
+--|     index into the array, and the result is stored as the value at that 
+--|     index.
+--|
+--|     Because the encoder state is stored in a union, we can use this to 
+--|     directly index into the below array, and retrieve the correct counter 
+--|     increment for the given encoder state.
+--|----------------------------------------------------------------------------|
 */
 const int8_t STATE_TRANSITIONS[16u] = 
 {
@@ -55,10 +98,27 @@ const int8_t STATE_TRANSITIONS[16u] =
      0, -1,  1,  0
 };
 
+/*
+--|----------------------------------------------------------------------------|
+--| PRIVATE VARIABLES
+--|----------------------------------------------------------------------------|
+*/
 
- /*-----------------------------------------------------------------------------------------------
-    BSP_Rotary_Encoder Function Definitions
- -------------------------------------------------------------------------------------------------*/
+/* None */
+
+/*
+--|----------------------------------------------------------------------------|
+--| PRIVATE HELPER FUNCTION PROTOTYPES
+--|----------------------------------------------------------------------------|
+*/
+
+/* None */
+
+/*
+--|----------------------------------------------------------------------------|
+--| PUBLIC FUNCTION DEFINITIONS
+--|----------------------------------------------------------------------------|
+*/
 
 void BSP_Rotary_Encoder_Initialize(BSP_Rotary_Encoder_t * pEncoder)
 {
@@ -67,7 +127,6 @@ void BSP_Rotary_Encoder_Initialize(BSP_Rotary_Encoder_t * pEncoder)
     pEncoder->count = 0u;
     pEncoder->state.Raw_Data = 0u;
 }
-
 
 void BSP_Poll_Rotary_Encoder(BSP_Rotary_Encoder_t * pEncoder)
 {
@@ -82,3 +141,11 @@ void BSP_Poll_Rotary_Encoder(BSP_Rotary_Encoder_t * pEncoder)
     // get the increment from the state transitions array and increment the counter
     pEncoder->count += STATE_TRANSITIONS[pEncoder->state.Raw_Data];
 }
+
+/*
+--|----------------------------------------------------------------------------|
+--| PRIVATE HELPER FUNCTION DEFINITIONS
+--|----------------------------------------------------------------------------|
+*/
+
+/* None */
